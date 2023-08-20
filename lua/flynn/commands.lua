cad5184313422f65e532359f9c09e2383ae0dfac
Flynn Duniho <flynn@harveyosity.org>
2023-08-19 23:53:11 -0700

    Make a bash script template

--- lua/flynn/commands.lua
+++ lua/flynn/commands.lua
@@ -1,1 +1,6 @@
 vim.api.nvim_create_user_command('SoSync', 'w | so | PackerSync', {})
+local script_dir = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
+
+vim.cmd(string.format([[
+   autocmd BufNewFile *.sh silent! 0r %s/../../templates/bash.sh
+]], script_dir))
