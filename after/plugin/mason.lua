05a0cad95f09de82b3525adc255a82e4470cb28e
Flynn Duniho <flynn@harveyosity.org>
2023-10-25 23:28:32 -0400

    Revert "finish Mason setup"
    This reverts commit 05ef4b8d5973fce1709260b9e665d762f49c80bb.

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,24 +1,15 @@
-local servers = {
-	"clangd",
-	"csharp_ls",
-	"cssls",
-	"html",
-	"jsonls",
-	"ltex",
-	"lua_ls",
-	"powershell_es",
-	"pyright",
-	"tsserver",
-}
-
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = servers,
-	automatic_installation = true,
+	ensure_installed = {
+    "clangd",
+    "csharp_ls",
+    "cssls",
+    "html",
+    "jsonls",
+    "ltex",
+    "lua_ls",
+    "powershell_es",
+    "pyright",
+    "tsserver",
+	},
 }
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-for _, v in pairs(servers) do
-	require('lspconfig')[v].setup { capabilities = capabilities }
-end
-
