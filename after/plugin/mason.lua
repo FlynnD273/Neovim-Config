25b400c79b39d895ded2d82c70b5c7e0e6b5d478
Flynn Duniho <flynn@harveyosity.org>
2023-10-25 23:28:40 -0400

    Revert "setup mason lspconfig"
    This reverts commit 671eb36e25fb7e831ba699206978e1ffd6e26ddf.

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,15 +1,1 @@
 require("mason").setup()
-require("mason-lspconfig").setup {
-	ensure_installed = {
-    "clangd",
-    "csharp_ls",
-    "cssls",
-    "html",
-    "jsonls",
-    "ltex",
-    "lua_ls",
-    "powershell_es",
-    "pyright",
-    "tsserver",
-	},
-}
