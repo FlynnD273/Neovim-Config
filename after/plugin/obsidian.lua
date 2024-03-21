7d831ffa858a9c37625de9f30b84c849665ebd84
Flynn Duniho <flynn@harveyosity.org>
2024-03-21 11:57:29 -0400

    update settings

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -57,6 +57,7 @@ require("obsidian").setup({
 		date_format = "%Y/%m-%B/%Y-%m-%d %a"
 	},
 
+	new_notes_location = "current_dir",
 	-- Optional, completion.
 	completion = {
 		-- If using nvim-cmp, otherwise set to false
@@ -66,7 +67,6 @@ require("obsidian").setup({
 		-- Where to put new notes created from completion. Valid options are
 		--  * "current_dir" - put new notes in same directory as the current buffer.
 		--  * "notes_subdir" - put new notes in the default notes subdirectory.
-		new_notes_location = "current_dir"
 	},
 
 	-- Optional, customize how names/IDs for new notes are created.
--- lua/flynn/packer.lua
+++ lua/flynn/packer.lua
@@ -72,7 +72,7 @@ return require('packer').startup(function(use)
 		requires = 'nvim-lua/plenary.nvim',
 	}
 	use 'theHamsta/nvim-dap-virtual-text'
-	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } }
 	use_rocks 'luafilesystem'
 	-- use 'github/copilot.vim'
 
