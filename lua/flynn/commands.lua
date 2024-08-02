10a46618a38652b0ae2ec30ec518ec3ee8f7f691
Flynn Duniho <flynn@harveyosity.org>
2024-08-02 15:06:42 -0400

    Set up template correctly

--- lua/flynn/commands.lua
+++ lua/flynn/commands.lua
@@ -4,38 +4,41 @@ local script_dir = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
 vim.cmd(string.format([[
    autocmd BufNewFile *.sh silent! 0r %s/../../templates/bash.sh
 ]], script_dir))
+vim.cmd(string.format([[
+   autocmd BufNewFile *.desktop silent! 0r %s/../../templates/desktop.desktop
+]], script_dir))
 
 function Execute_command(command)
-    local tmpfile = os.tmpname()
-    local exit = os.execute(command .. ' > ' .. tmpfile .. ' 2> ' .. tmpfile .. '.err')
+  local tmpfile = os.tmpname()
+  local exit = os.execute(command .. ' > ' .. tmpfile .. ' 2> ' .. tmpfile .. '.err')
 
-    local stdout_file = io.open(tmpfile)
-    local stdout = stdout_file:read("*all")
+  local stdout_file = io.open(tmpfile)
+  local stdout = stdout_file:read("*all")
 
-    local stderr_file = io.open(tmpfile .. '.err')
-    local stderr = stderr_file:read("*all")
+  local stderr_file = io.open(tmpfile .. '.err')
+  local stderr = stderr_file:read("*all")
 
-    stdout_file:close()
-    stderr_file:close()
+  stdout_file:close()
+  stderr_file:close()
 
-    return exit, stdout, stderr
+  return exit, stdout, stderr
 end
 
 function Compile_tex()
-	local tex_file = vim.fn.expand('%:p') -- Get the full path of the current file
-	local dir = ""
-	if package.config:sub(1, 1) == '\\' then
-			dir = tex_file:match("(.*\\)")
-	else
-			dir = tex_file:match("(.*/)")
-	end
+  local tex_file = vim.fn.expand('%:p') -- Get the full path of the current file
+  local dir = ""
+  if package.config:sub(1, 1) == '\\' then
+    dir = tex_file:match("(.*\\)")
+  else
+    dir = tex_file:match("(.*/)")
+  end
 
-	local cmd =  'pdflatex -interaction=nonstopmode "' .. tex_file .. '"' .. ' -output-directory="' .. dir .. '"'
-	local exit_code, stdout, stderr = Execute_command(cmd)
+  local cmd = 'pdflatex -interaction=nonstopmode "' .. tex_file .. '"' .. ' -output-directory="' .. dir .. '"'
+  local exit_code, stdout, stderr = Execute_command(cmd)
 
-	if exit_code ~= 0 then
-		print(stdout)
-	end
+  if exit_code ~= 0 then
+    print(stdout)
+  end
 end
 
 -- Set up an autocommand to trigger the compile_tex function on BufWritePost for .tex files
