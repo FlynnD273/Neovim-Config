044a41b40967ddb73f715db6e4ee116fe8338b2f
Flynn Duniho <flynn@harveyosity.org>
2024-10-05 01:32:59 -0400

    Update fzf native installation command

--- lua/flynn/packer.lua
+++ lua/flynn/packer.lua
@@ -32,8 +32,7 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = 'nvim-lua/plenary.nvim',
   }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run =
-  'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'BurntSushi/ripgrep'
   use 'sharkdp/fd'
 
