fbe58f04609201de000da96c0c3f49bf9adc8960
Flynn Duniho <flynn@harveyosity.org>
2024-10-07 10:43:07 -0400

    Update DAP keymap

--- after/plugin/dap-ui.lua
+++ after/plugin/dap-ui.lua
@@ -1,3 +1,7 @@
 require("dapui").setup()
 
-vim.keymap.set("n", "<leader>d", ':lua require("dapui").toggle()<CR>')
+vim.keymap.set("n", "<leader>dt", ":lua require('dapui').toggle()<CR>")
+vim.keymap.set("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<leader>8", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<leader>9", ":lua require'dap'.step_into()<CR>")
