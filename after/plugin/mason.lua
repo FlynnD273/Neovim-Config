1e32d5d03ec2e19bbea386d80d19b32e08790b39
Flynn Duniho <flynn@harveyosity.org>
2024-12-17 18:37:57 -0500

    Switch to CSharp_LS for C#

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -3,68 +3,12 @@ require("mason-lspconfig").setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 require("mason-lspconfig").setup_handlers {
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
   function(server_name) -- default handler (optional)
     lspconfig[server_name].setup { capabilities = capabilities, }
   end,
-  -- Next, you can provide a dedicated handler for specific servers.
-  -- For example, a handler override for the `rust_analyzer`:
-  ["omnisharp"] = function()
-    lspconfig.omnisharp.setup {
-      handlers = {
-        ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
-        ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
-        ["textDocument/references"] = require('omnisharp_extended').references_handler,
-        ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
-      },
-      capabilities = capabilities,
-      cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-      settings = {
-        FormattingOptions = {
-          -- Enables support for reading code style, naming convention and analyzer
-          -- settings from .editorconfig.
-          EnableEditorConfigSupport = true,
-          -- Specifies whether 'using' directives should be grouped and sorted during
-          -- document formatting.
-          OrganizeImports = true,
-        },
-        MsBuild = {
-          -- If true, MSBuild project system will only load projects for files that
-          -- were opened in the editor. This setting is useful for big C# codebases
-          -- and allows for faster initialization of code navigation features only
-          -- for projects that are relevant to code that is being edited. With this
-          -- setting enabled OmniSharp may load fewer projects and may thus display
-          -- incomplete reference lists for symbols.
-          LoadProjectsOnDemand = nil,
-        },
-        RoslynExtensionsOptions = {
-          -- Enables support for roslyn analyzers, code fixes and rulesets.
-          EnableAnalyzersSupport = true,
-          -- Enables support for showing unimported types and unimported extension
-          -- methods in completion lists. When committed, the appropriate using
-          -- directive will be added at the top of the current file. This option can
-          -- have a negative impact on initial completion responsiveness,
-          -- particularly for the first few completion sessions after opening a
-          -- solution.
-          EnableImportCompletion = true,
-          -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-          -- true
-          AnalyzeOpenDocumentsOnly = false,
-        },
-        Sdk = {
-          -- Specifies whether to include preview versions of the .NET SDK when
-          -- determining which version to use for project loading.
-          IncludePrereleases = false,
-        },
-      },
-      root_dir = function(startpath)
-        return lspconfig.util.root_pattern("*.sln")(startpath)
-            or lspconfig.util.root_pattern("*.csproj")(startpath)
-            or lspconfig.util.root_pattern("*.fsproj")(startpath)
-            or lspconfig.util.root_pattern(".git")(startpath)
-      end,
-    }
-  end
+
+  -- ["omnisharp"] = function()
+  --   lspconfig.omnisharp.setup {
+  --   }
+  -- end
 }
