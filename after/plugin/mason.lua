dbc44837acef9eee9ac6afff14a203cbc044b5fd
Flynn Duniho <flynn@harveyosity.org>
2024-12-18 16:24:21 -0800

    Switch to CSharp_LS

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -3,12 +3,17 @@ require("mason-lspconfig").setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 require("mason-lspconfig").setup_handlers {
-  function(server_name) -- default handler (optional)
+  function(server_name)
     lspconfig[server_name].setup { capabilities = capabilities, }
   end,
 
-  -- ["omnisharp"] = function()
-  --   lspconfig.omnisharp.setup {
-  --   }
-  -- end
+  ["csharp_ls"] = function()
+    local path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+    local root_file = vim.fs.find({"*.sln", "*.csproj", "*.fsproj", ".git"}, { type = "file", path = path, upward = true })[1]
+    path = vim.fs.dirname(root_file)
+    lspconfig.csharp_ls.setup {
+      capabilities = capabilities,
+      root_dir = path,
+    }
+  end
 }
