ed80fb6367b5b4f89a3b81252af7b4cf3e580279
Flynn Duniho <flynn@harveyosity.org>
2025-03-17 17:42:25 -0400

    Add Racket support

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,7 +2,8 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
-lspconfig["gdscript"].setup { capabilities = capabilities }
+lspconfig.gdscript.setup { capabilities = capabilities }
+lspconfig.racket_langserver.setup { capabilities = capabilitiep }
 require("mason-lspconfig").setup_handlers {
   function(server_name)
     lspconfig[server_name].setup { capabilities = capabilities, }
