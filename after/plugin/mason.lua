fce6381002edcfa55f9805edb522bbffdbd5e626
Flynn Duniho <flynn@harveyosity.org>
2025-05-07 15:19:14 -0400

    Need to migrate to new LSP config

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -4,7 +4,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 lspconfig.gdscript.setup { capabilities = capabilities }
 -- raco pkg install racket-langserver
-lspconfig.racket_langserver.setup { capabilities = capabilitiep }
+lspconfig.racket_langserver.setup { capabilities = capabilities }
 require("mason-lspconfig").setup_handlers {
   function(server_name)
     lspconfig[server_name].setup { capabilities = capabilities, }
