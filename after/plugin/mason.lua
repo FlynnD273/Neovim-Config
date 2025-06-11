06fa8fc75124f0206155d408351e3b0084345987
Flynn Duniho <flynn@harveyosity.org>
2025-06-10 18:06:55 -0700

    Prevent Lua LS from starting twice

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -5,43 +5,45 @@ local lspconfig = require('lspconfig')
 lspconfig.gdscript.setup { capabilities = capabilities }
 -- raco pkg install racket-langserver
 lspconfig.racket_langserver.setup { capabilities = capabilities }
-require 'lspconfig'.lua_ls.setup {
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
-				return
-			end
-		end
+-- lspconfig.lua_ls.setup {
+-- 	on_init = function(client)
+-- 		if client.workspace_folders then
+-- 			local path = client.workspace_folders[1].name
+-- 			if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+-- 				return
+-- 			end
+-- 		end
 
-		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-			runtime = {
-				-- Tell the language server which version of Lua you're using
-				-- (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT'
-			},
-			-- Make the server aware of Neovim runtime files
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME
-					-- Depending on the usage, you might want to add additional paths here.
-					-- "${3rd}/luv/library"
-					-- "${3rd}/busted/library",
-				}
-				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-				-- library = vim.api.nvim_get_runtime_file("", true)
-			}
-		})
-	end,
-	settings = {
-		Lua = {}
-	}
-}
+-- 		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+-- 			runtime = {
+-- 				-- Tell the language server which version of Lua you're using
+-- 				-- (most likely LuaJIT in the case of Neovim)
+-- 				version = 'LuaJIT'
+-- 			},
+-- 			-- Make the server aware of Neovim runtime files
+-- 			workspace = {
+-- 				checkThirdParty = false,
+-- 				library = {
+-- 					vim.env.VIMRUNTIME
+-- 					-- Depending on the usage, you might want to add additional paths here.
+-- 					-- "${3rd}/luv/library"
+-- 					-- "${3rd}/busted/library",
+-- 				}
+-- 				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+-- 				-- library = vim.api.nvim_get_runtime_file("", true)
+-- 			}
+-- 		})
+-- 	end,
+-- 	settings = {
+-- 		Lua = {}
+-- 	}
+-- }
+
 local path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
 local root_file = vim.fs.find({ "*.sln", "*.csproj", "*.fsproj", ".git" },
 	{ type = "file", path = path, upward = true })[1]
 path = vim.fs.dirname(root_file)
+
 lspconfig.omnisharp.setup {
 	handlers = { ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
 		["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
