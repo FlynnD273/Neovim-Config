fa56363361b366978a701498c26bcdae3db5da8d
Flynn Duniho <flynn@harveyosity.org>
2025-06-20 15:26:49 -0700

    Add a printerr snippet for GDScript

--- lua/flynn/cmp.lua
+++ lua/flynn/cmp.lua
@@ -2,56 +2,57 @@ local cmp = require 'cmp'
 -- local luasnip = require'luasnip'
 
 -- vim.keymap.set('i', '<C-m>', 'noop')
+require("luasnip.loaders.from_snipmate").lazy_load()
 
 cmp.setup({
-  view = {
-    entries = "native"
-  },
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+	view = {
+		entries = "native"
+	},
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-    ['<M-k>'] = cmp.mapping.scroll_docs(-4),
-    ['<M-j>'] = cmp.mapping.scroll_docs(4),
-    ['<M-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-    { name = 'path' },
-    { name = 'buffer' },
-  })
+		['<M-k>'] = cmp.mapping.scroll_docs(-4),
+		['<M-j>'] = cmp.mapping.scroll_docs(4),
+		['<M-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp_signature_help' },
+		{ name = 'nvim_lsp' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+		{ name = 'path' },
+		{ name = 'buffer' },
+	})
 })
 
 -- Set configuration for specific filetype.
@@ -65,25 +66,25 @@ cmp.setup({
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
-  view = {
-    entries = "custom"
-  },
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+	view = {
+		entries = "custom"
+	},
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  view = {
-    entries = "custom"
-  },
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-    { name = 'cmdline' },
-  })
+	view = {
+		entries = "custom"
+	},
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' },
+		{ name = 'cmdline' },
+	})
 })
 
 -- If you want insert `(` after select function or method item
@@ -199,13 +200,13 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- require 'lspconfig'.glslls.setup { capabilities = capabilities }
 
 if vim.is_windows then
-  local home = vim.env.HOME
-  if home == nil then
-    home = os.getenv("HOME")
-  end
-  -- require 'lspconfig'.powershell_es.setup { -- https://github.com/PowerShell/PowerShellEditorServices/releases
-  --   bundle_path = home .. [[/PowerShellEditorServices]],
-  -- }
+	local home = vim.env.HOME
+	if home == nil then
+		home = os.getenv("HOME")
+	end
+	-- require 'lspconfig'.powershell_es.setup { -- https://github.com/PowerShell/PowerShellEditorServices/releases
+	--   bundle_path = home .. [[/PowerShellEditorServices]],
+	-- }
 end
 
 
